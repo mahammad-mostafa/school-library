14b61da207b525f1bb436de51919304357761237
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-10-22 02:06:54 +0300

    Added method for class association

--- person_class.rb
+++ person_class.rb
@@ -2,12 +2,17 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
+  end
+
+  def add_rental(rental)
+    @rentals.append(rental) unless rentals.include?(rental)
   end
 
   def can_use_services?
